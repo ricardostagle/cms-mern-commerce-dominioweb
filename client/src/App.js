ede6f535b7af44f76593d45239e177d78d8ecddf
ricardostagle <ricardo.stagle@gmail.com>
2023-09-14 09:36:32 -0600

    updated setting github actions for firebase

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,6 @@ import Main from './components/Main';
 import store from './store';
 import {loadUser} from './actions/authActions';
 import { BrowserRouter } from 'react-router-dom';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAeo_FGKWbeowNN-ScI3TDtbyGn3HR8OaY",
-  authDomain: "dominiowebcommx-b6ee1.firebaseapp.com",
-  projectId: "dominiowebcommx-b6ee1",
-  storageBucket: "dominiowebcommx-b6ee1.appspot.com",
-  messagingSenderId: "60683105886",
-  appId: "1:60683105886:web:9e70405ca3660d06bfe6cf",
-  measurementId: "G-24X7JES76G"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 class App extends Component {
   componentDidMount(){
--- server.js
+++ server.js
@@ -8,6 +8,27 @@ const itemRoutes = require('./routes/item');
 const cartRoutes = require('./routes/cart');
 const orderRoutes = require('./routes/order');
 const userRoutes = require('./routes/user');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAeo_FGKWbeowNN-ScI3TDtbyGn3HR8OaY",
+  authDomain: "dominiowebcommx-b6ee1.firebaseapp.com",
+  projectId: "dominiowebcommx-b6ee1",
+  storageBucket: "dominiowebcommx-b6ee1.appspot.com",
+  messagingSenderId: "60683105886",
+  appId: "1:60683105886:web:9e70405ca3660d06bfe6cf",
+  measurementId: "G-24X7JES76G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const app = express();
 app.use(express.json());
@@ -18,12 +39,16 @@ app.use('/api',cartRoutes);
 app.use('/api',orderRoutes);
 app.use('/api',userRoutes);
 
-if(process.env.NODE_ENV === 'production') {
+//if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+      //res.send(req.query);
     });
-}
+    app.listen(port, () => {
+        console.log("hey app is listening");
+    });
+//}
 
 const dbURI = config.get('dbURI');
 const port = process.env.PORT || 4000;
